3f31783f9176d5c84e31a1348330eff8262c9c41
Alison Leme <alisonletos@hotmail.com>
2022-04-28 13:43:46 -0300

    Adicionando feature de carrosel com react-material-ui-carousel

--- pages/product.js
+++ pages/product.js
@@ -1,3 +1,5 @@
+import Carousel from 'react-material-ui-carousel'
+
 import {
     Grid,
     Box,
@@ -21,7 +23,31 @@ const Product = () => {
                 <Grid container spacing={3}>
                     <Grid item xs={8}>
                         <Box className={styles.box}>
-                            Carrossel
+                            <Carousel
+                                autoPlay={true}
+                                animation={'slide'}
+                                navButtonsAlwaysVisible={true}
+                                navButtonsProps={{
+                                    style: {
+                                        color: 'white'
+                                    }
+                                }}
+                            >
+                                <Card className={styles.card}>
+                                    <CardMedia
+                                        className={styles.cardMedia}
+                                        image={'https://source.unsplash.com/random?a=1'}
+                                        title={'Title image'}
+                                    />
+                                </Card>
+                                <Card className={styles.card}>
+                                    <CardMedia
+                                        className={styles.cardMedia}
+                                        image={'https://source.unsplash.com/random?a=2'}
+                                        title={'Title image'}
+                                    />
+                                </Card>
+                            </Carousel>
                         </Box>
                         <Box className={styles.box}>
                             <Typography component={'span'} variant={'caption'}>
@@ -40,18 +66,18 @@ const Product = () => {
                                 Descrição
                             </Typography>
                             <Typography component={'p'} variant={'body2'}>
-                                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.
                             </Typography>
                         </Box>
                     </Grid>
                     <Grid item xs={4}>
                         <Card className={styles.box} elevation={0}>
-                            <CardHeader 
+                            <CardHeader
                                 avatar={<Avatar>A</Avatar>}
                                 title={'Alison Leme'}
                                 subheader={'alisonletos@gmail.com'}
                             />
-                            <CardMedia 
+                            <CardMedia
                                 image={'https://source.unsplash.com/random'}
                                 title={'Alison Leme'}
                             />
